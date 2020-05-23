2f3008e89a721c51e4fbdcb3b37b0fcad73495fb
Ryan Bush <rpbush96@gmail.com>
2020-05-23 15:29:51 +0100

    v0.2.1

--- app/js/listSurveys.js
+++ app/js/listSurveys.js
@@ -9,19 +9,28 @@ function showAllSurveys() {
 async function showTable() {
     let surveyElement = document.getElementById('surveys');
     let table = document.createElement('table');
+    let tableHead = document.createElement('thead');
+    let tableBody = document.createElement('tbody');
     let tR = document.createElement('tr');
+    tR.class = 'tableHead';
     let tH1 = document.createElement('th');
+    tH1.class = 'tableColumn1'
     let tH2 = document.createElement('th');
+    tH2.class = 'tableColumn2'
     let tH3 = document.createElement('th');
+    tH3.class = 'tableColumn3'
     let tH4 = document.createElement('th');
+    tH4.class = 'tableColumn4'
     let tH5 = document.createElement('th');
+    tH5.class = 'tableColumn5'
     let tH11 = document.createTextNode('Survey ID');
     let tH21 = document.createTextNode('Survey Name');
     let tH31 = document.createTextNode('Survey Link');
     let tH41 = document.createTextNode('Survey Responses');
     let tH51 = document.createTextNode('Date');
 
-    table.appendChild(tR);
+    table.appendChild(tableHead);
+    tableHead.appendChild(tR);
     tR.appendChild(tH1);
     tR.appendChild(tH2);
     tR.appendChild(tH3);
@@ -32,14 +41,20 @@ async function showTable() {
     tH3.appendChild(tH31);
     tH4.appendChild(tH41);
     tH5.appendChild(tH51);
+    table.appendChild(tableBody);
 
     for (let i = 0; i < surveys.length; i++) {
         let tRow = document.createElement('tr');
         let tD1 = document.createElement('td');
+        tD1.class = 'tableColumn1';
         let tD2 = document.createElement('td');
+        tD2.class = 'tableColumn2';
         let tD3 = document.createElement('td');
+        tD3.class = 'tableColumn3';
         let tD4 = document.createElement('td');
+        tD4.class = 'tableColumn4';
         let tD5 = document.createElement('td');
+        tD5.class = 'tableColumn5';
         let tD11 = document.createTextNode(surveys[i].id);
         let name = await getSurveyName(surveys[i].id);
         let tD21 = document.createTextNode(name);
@@ -50,7 +65,7 @@ async function showTable() {
         let tD42 = document.createTextNode("View Responses");
         tD41.href = "results#" + surveys[i].id;
         let tD51 = document.createTextNode(surveys[i].time);
-        table.appendChild(tRow);
+        tableBody.appendChild(tRow);
         tRow.appendChild(tD1);
         tRow.appendChild(tD2);
         tRow.appendChild(tD3);
@@ -89,6 +104,15 @@ async function loadAllSurveys() {
         surveys = {msg: 'failed to load survey :-('};
     }
     showAllSurveys();
+}
+
+function navigationCollapse() {
+    let x = document.getElementById("mainNav");
+    if (x.className === "mainNav") {
+        x.className += " responsive";
+    } else {
+        x.className = "mainNav";
+    }
 }
 
 function pageLoaded() {
